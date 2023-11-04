9591d9ef404904470ce3668e77846f2caab3653f
Ayuna <116560592+ayuna-stpyko@users.noreply.github.com>
2023-11-04 17:17:47 +0900

    Update bingo.py

--- bingo.py
+++ bingo.py
@@ -1,16 +1,12 @@
-from random import randint, choice
+from random import randint, sample
 
-def make_number(selected_number: set[int], unselected_number: set[int]) -> int:
+def make_number(number_order: list[int], opened: int) -> tuple[int, int]:
     """
     がちゃがちゃ
-    selected_numberはすでに発表された数字
-    unselected_numberはまだ発表されていない数字
-    まだ発表されていない数字のうちランダムに数字1つが発表される
+    まだ発表されていない数字が1つ発表される
     """
-    x = choice(list(selected_number))
-    unselected_number.add(x)
-    selected_number.remove(x)
-    return x
+    x = number_order[opened]
+    return x, opened + 1
 
 def make_bingo() -> tuple[tuple[tuple[int]], tuple[tuple[int]]]:
     """
@@ -32,12 +28,58 @@ def make_bingo() -> tuple[tuple[tuple[int]], tuple[tuple[int]]]:
     _num_to_ind_tuple = tuple(pair for pair in _num_to_ind_list)
     return _ind_to_num_tuple, _num_to_ind_tuple
 
+def count(new_number: int, num_to_ind: tuple[tuple[int]], cnt: list[int], cnt_to_ind: tuple[tuple[tuple[int]]]):
+    """
+    もしビンゴ内に数字があればカウントを増やす
+    リーチ、ビンゴがあればそれを示す
+    """
+    i, j = num_to_ind[new_number]
+    if i == -1:
+        return
+    cnt[i] += 1
+    cnt[j + 5] += 1
+    if i == j:
+        cnt[10] += 1
+    if i + j == 4:
+        cnt[11] += 1
+    print(cnt)
+    for k in range(12):
+        if cnt[k] == 4:
+            for i, j in cnt_to_ind[k]:
+                # リーチである目印(色変えるなど)
+                pass
+    for k in range(12):
+        if cnt[k] == 5:
+            for i, j in cnt_to_ind[k]:
+                # ビンゴである目印(色変えるなど)
+                pass
+
 def make_red(new_number: int) -> None:
     # <span>を加えて赤色にする
     pass
 
-if __name__=="__main__":
-    ind_to_num, num_to_ind = make_bingo()
-    selected_number = set(i for i in range(1, 101))
-    unselected_number = set()
-    new_number = make_number(selected_number, unselected_number)
+ind_to_num, num_to_ind = make_bingo()
+
+number_order = sample([i for i in range(1, 101)], 100)
+opened = 0
+
+cnt = [0 for _ in range(12)]
+cnt_to_ind = (((0, 0), (0, 1), (0, 2), (0, 3), (0, 4)),
+              ((1, 0), (1, 1), (1, 2), (1, 3), (1, 4)),
+              ((2, 0), (2, 1), (2, 2), (2, 3), (2, 4)),
+              ((3, 0), (3, 1), (3, 2), (3, 3), (3, 4)),
+              ((4, 0), (4, 1), (4, 2), (4, 3), (4, 4)),
+              ((0, 0), (1, 0), (2, 0), (3, 0), (4, 0)),
+              ((0, 1), (1, 1), (2, 1), (3, 1), (4, 1)),
+              ((0, 2), (1, 2), (2, 2), (3, 2), (4, 2)),
+              ((0, 3), (1, 3), (2, 3), (3, 3), (4, 3)),
+              ((0, 4), (1, 4), (2, 4), (3, 4), (4, 4)),
+              ((0, 0), (1, 1), (2, 2), (3, 3), (4, 4)),
+              ((0, 4), (1, 3), (2, 2), (3, 1), (4, 0)))
+
+print(ind_to_num)
+for i in range(50):
+    new_number, opened = make_number(number_order, opened)
+    print(new_number)
+    count(new_number, num_to_ind, cnt, cnt_to_ind)
+    make_red(new_number)
