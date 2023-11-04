0132fbd90e83be473300b929d55c32e8fcb727f6
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-11-04 04:53:07 +0000

    fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('body.html', name='sample')
+    return render_template('body.html', name='Title')
 
 @app.route('/user/<username>')
 def profile(username):
